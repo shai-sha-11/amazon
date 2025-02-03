8f119a0a993bc3a31c43ee40f98fdf647c8dc28f
shai-sha-11 <shaheenchoori783@gmail.com>
2025-02-03 21:37:59 +0530

    Add files via upload

---
+++ scripts/checkout.js
@@ -0,0 +1,7 @@
+import { renderOrderSummary } from "./chekout/orderSummary.js";
+import { renderPaymentSummary } from "./chekout/paymentSummary.js";
+// import '../data/cart-class.js';
+import '../data/backend-practice.js';
+
+renderOrderSummary();
+renderPaymentSummary();
